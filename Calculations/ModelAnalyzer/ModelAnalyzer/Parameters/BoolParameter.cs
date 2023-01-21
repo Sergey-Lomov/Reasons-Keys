cd7cde4cc4c4fb096b49afb3a6f3df2797190a27
Sergey Lomov <sspirit10000@gmail.com>
2023-01-21 03:15:37 +0300

    Added boolean parameter

---
+++ Calculations/ModelAnalyzer/ModelAnalyzer/Parameters/BoolParameter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelAnalyzer.Parameters
+{
+    class BoolParameter : Parameter
+    {
+        readonly private string nullValueStub = "-";
+
+        protected bool? value = null;
+
+        public override bool IsValueNull()
+        {
+            return !value.HasValue;
+        }
+
+        public override void SetupByString(string str)
+        {
+            try
+            {
+                value = bool.Parse(str)
+            }
+            catch
+            {
+                value = null;
+            }
+        }
+
+        public override string StringRepresentation()
+        {
+            return value.HasValue ? value.ToString() : nullValueStub;
+        }
+
+        protected override void NullifyValue()
+        {
+            value = null;
+        }
+    }
+}
